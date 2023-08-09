19afd9f37ca67291a23b027d912e5f29f828680f
viktorfilmanovich <film.viktor@yandex.ru>
2023-08-09 22:37:08 +0300

    .

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,20 @@
-//= components/script.js
-//= components/script2.js
-//= components/script3.js
+const openButton = document.getElementById("openButton");
+const closeButton = document.getElementById("closeButton");
+const mobileMenu = document.getElementById("mobileMenu");
+const menuLinks = mobileMenu.querySelectorAll("ul > li");
+
+console.log(menuLinks.length);
+
+openButton.addEventListener("click", () => {
+  mobileMenu.classList.add("mobile-menu--open");
+});
+
+closeButton.addEventListener("click", () => {
+  mobileMenu.classList.remove("mobile-menu--open");
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("mobile-menu--open");
+  });
+});
